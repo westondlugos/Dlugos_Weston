ef1be5de0e06d1c0b6e6a56436f913f9f606e163
westondlugos <DlugosW2064@sduhsd.lan>
2016-09-07 09:45:22 -0700

    ProgrammingLesson_02

---
+++ ProgrammingLesson_02/Lab_02.py
@@ -0,0 +1,42 @@
+#multiplication
+
+
+A = 17
+B = 2
+
+C = A * B
+
+print(" The answer to",A,"times",B,"is",C)
+
+#concatenation
+
+print("")
+print("Address Label")
+print("")
+      
+
+Name = "Weston Dlugos" 
+Address = "16907 Via De Santa Fe"
+City = "Rancho Santa Fe"
+zipCode = 92067
+
+print("",Name)
+print("",Address)
+print("",City)
+print("",zipCode)
+
+print('')
+print('Surface Area')
+print('')
+
+L = 6
+W = 7
+H = 10
+
+surfaceArea = (2 * L * W) + (2 * W * H) + (2 * L * H)
+print("",surfaceArea,"cm^2")
+
+
+
+
+
---
+++ ProgrammingLesson_02/PyLesson02.py
@@ -0,0 +1,7 @@
+L = 10
+W = 5
+
+recArea = L * W
+
+print (" the area of the rectangle is", recArea)
+    
