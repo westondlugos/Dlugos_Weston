7441e437e98fd298b2c9885ee82b3dddc311715c
westondlugos <dlugosweston@gmail.com>
2017-02-03 09:38:21 -0800

    ProgrammingLesson_10

--- ProgrammingLesson_10/divisor.py
+++ ProgrammingLesson_10/divisor.py
@@ -13,7 +13,7 @@ for nums in numsList:
     output += "\n"
     for num in nums:
         output += str(num) + "\t"
-
+print(output)
 div = int(input("Please enter a number:"))
 
 
@@ -26,7 +26,7 @@ for nums in numsList:
         if num % div == 0:
             count += 1
 
-print(output)
+
 
 print("There are", count, "numbers divisible by", div, "in the list")
     
