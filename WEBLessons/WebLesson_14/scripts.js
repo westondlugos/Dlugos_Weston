e6545cf5602845f748b607c2cf4e7e02ba2f79f0
westondlugos <dlugosweston@gmail.com>
2017-04-04 09:34:47 -0700

    WebLesson_14

---
+++ WEBLessons/WebLesson_14/scripts.js
@@ -0,0 +1,17 @@
+function emailValidation(){
+    var x = document.forms.input.email.value;
+    var atPos =  x.indexOf("@");
+    var dotPos = x.lastIndexOf(".");
+    if( atPos < 1 ||  dotPos < atPos + 2 || dotPos + 2 > x.length)
+        alert("Email isn't valid");
+    else
+        alert("Email valid");
+}
+function passwordValidation(){
+    var y = document.forms.input.password.value;
+
+    if( y.length < 6)
+        alert("password isn't valid");
+    else
+        alert("password valid");
+}
